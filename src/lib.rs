37fd258ab936e8a74437417d02be7aa9ab7160a1
Cheney Lin <lincheney@gmail.com>
2017-04-16 13:35:46 +1000

    override forward search as well

--- src/lib.rs
+++ src/lib.rs
@@ -34,34 +34,56 @@ mod readline {
         }
     }
 
+    pub fn refresh_line() {
+        unsafe{ rl_refresh_line(0, 0) };
+    }
+
+    pub fn set_text(buf: Vec<u8>) {
+        let ptr = buf.as_ptr();
+        unsafe {
+            // clear line
+            rl_end_of_line(0, 0);
+            rl_unix_line_discard(0, 0);
+            rl_refresh_line(0, 0);
+            // insert selected
+            rl_insert_text(ptr);
+        }
+    }
+
     #[link(name = "readline")]
     extern {
         fn history_list() -> *const *const HistEntry;
-        pub fn rl_unix_line_discard(count: isize, key: isize) -> isize;
-        pub fn rl_refresh_line(count: isize, key: isize) -> isize;
-        pub fn rl_end_of_line(count: isize, key: isize) -> isize;
-        pub fn rl_insert_text(string: *const u8) -> isize;
+        fn rl_unix_line_discard(count: isize, key: isize) -> isize;
+        fn rl_refresh_line(count: isize, key: isize) -> isize;
+        fn rl_end_of_line(count: isize, key: isize) -> isize;
+        fn rl_insert_text(string: *const u8) -> isize;
     }
 
-    fn get_original_fn(name: &str) -> fn(isize, isize)->isize {
+    fn get_original_fn(name: &str) -> unsafe fn(isize, isize)->isize {
         let ptr = name.as_ptr();
         let func = unsafe{ ::libc::dlsym(::libc::RTLD_NEXT, ptr as *const i8) };
         unsafe{ ::std::mem::transmute(func) }
     }
 
     lazy_static! {
-        pub static ref RL_REVERSE_SEARCH_HISTORY: fn(isize, isize)->isize = get_original_fn("rl_reverse_search_history\0");
-        pub static ref RL_FORWARD_SEARCH_HISTORY: fn(isize, isize)->isize = get_original_fn("rl_forward_search_history\0");
+        pub static ref RL_REVERSE_SEARCH_HISTORY: unsafe fn(isize, isize)->isize = get_original_fn("rl_reverse_search_history\0");
+        pub static ref RL_FORWARD_SEARCH_HISTORY: unsafe fn(isize, isize)->isize = get_original_fn("rl_forward_search_history\0");
     }
 }
 
 #[no_mangle]
 pub extern fn rl_reverse_search_history(direction: isize, key: isize) -> isize {
-    // custom_isearch();
-    readline::RL_REVERSE_SEARCH_HISTORY(direction, key)
+    if custom_isearch() { return 0; }
+    unsafe{ readline::RL_REVERSE_SEARCH_HISTORY(direction, key) }
 }
 
-fn custom_isearch() {
+#[no_mangle]
+pub extern fn rl_forward_search_history(direction: isize, key: isize) -> isize {
+    if custom_isearch() { return 0; }
+    unsafe{ readline::RL_FORWARD_SEARCH_HISTORY(direction, key) }
+}
+
+fn custom_isearch() -> bool {
     let mut process = Command::new("fzf").arg("+m").arg("--tac").arg("--print0")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
@@ -75,8 +97,8 @@ fn custom_isearch() {
 
     process.stdin = Some(stdin);
     if ! process.wait().unwrap().success() {
-        unsafe{ readline::rl_refresh_line(0, 0); }
-        return
+        readline::refresh_line();
+        return true
     }
 
     let mut stdout = process.stdout.expect("could not open stdout");
@@ -85,13 +107,6 @@ fn custom_isearch() {
     // make sure buf is null terminated
     if *buf.last().unwrap_or(&1) != 0 { buf.push(0); }
 
-    let buf = buf.as_ptr();
-    unsafe {
-        // clear line
-        readline::rl_end_of_line(0, 0);
-        readline::rl_unix_line_discard(0, 0);
-        readline::rl_refresh_line(0, 0);
-        // insert selected
-        readline::rl_insert_text(buf);
-    }
+    readline::set_text(buf);
+    true
 }
