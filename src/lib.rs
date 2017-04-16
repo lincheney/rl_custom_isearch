2a42f0ee337eb5d2258a08afe982740ad0a84abf
Cheney Lin <lincheney@gmail.com>
2017-04-16 12:44:48 +1000

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,81 @@
+extern crate libc;
+
+use std::ffi::CStr;
+use std::os::raw::c_char;
+use std::io::{Write, Read};
+use std::process::{Command, Stdio};
+
+#[repr(C)]
+struct HistEntry {
+    line: *const c_char,
+    timestamp: *const c_char,
+    data: *const c_char,
+}
+
+impl HistEntry {
+    pub fn get_line<'a>(&'a self) -> &'a [u8] {
+        if self.line.is_null() { return &[0;0]; }
+        unsafe{ CStr::from_ptr((&self).line) }.to_bytes()
+    }
+}
+
+#[link(name = "readline")]
+extern {
+    fn history_list() -> *const *const HistEntry;
+    fn rl_unix_line_discard(count: isize, key: isize) -> isize;
+    fn rl_refresh_line(count: isize, key: isize) -> isize;
+    fn rl_end_of_line(count: isize, key: isize) -> isize;
+    fn rl_insert_text(string: *const u8) -> isize;
+}
+
+fn history_each<F>(mut callback: F) where F: FnMut(&[u8]) {
+    let mut history = unsafe{ history_list() };
+    if history.is_null() { return; }
+
+    while ! unsafe{ (*history) }.is_null() {
+        let entry = unsafe{ &**history };
+        callback(entry.get_line());
+        history = unsafe{ history.offset(1) };
+    }
+}
+
+#[no_mangle]
+pub extern fn rl_reverse_search_history(_direction: isize, _key: isize) -> isize {
+    custom_isearch();
+    return 0;
+}
+
+fn custom_isearch() {
+    let mut process = Command::new("fzf").arg("+m").arg("--tac").arg("--print0")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn().expect("could not find fzf");
+    let mut stdin = process.stdin.unwrap();
+
+    history_each(|line| {
+        stdin.write_all(line).unwrap();
+        stdin.write_all(b"\n").unwrap();
+    });
+
+    process.stdin = Some(stdin);
+    if ! process.wait().unwrap().success() {
+        unsafe{ rl_refresh_line(0, 0); }
+        return
+    }
+
+    let mut stdout = process.stdout.expect("could not open stdout");
+    let mut buf: Vec<u8> = vec![];
+    stdout.read_to_end(&mut buf).unwrap();
+    // make sure buf is null terminated
+    if *buf.last().unwrap_or(&1) != 0 { buf.push(0); }
+
+    let buf = buf.as_ptr();
+    unsafe {
+        // clear line
+        rl_end_of_line(0, 0);
+        rl_unix_line_discard(0, 0);
+        rl_refresh_line(0, 0);
+        // insert selected
+        rl_insert_text(buf);
+    }
+}
