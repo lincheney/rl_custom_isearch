03b745d29d1bd7f3ee22f56ba471e341b6c50550
Cheney Lin <lincheney@gmail.com>
2017-04-16 13:00:36 +1000

    move readline ffi inside mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,47 @@
 extern crate libc;
 
-use std::ffi::CStr;
-use std::os::raw::c_char;
 use std::io::{Write, Read};
 use std::process::{Command, Stdio};
 
-#[repr(C)]
-struct HistEntry {
-    line: *const c_char,
-    timestamp: *const c_char,
-    data: *const c_char,
-}
+mod readline {
+    use std::os::raw::c_char;
+    use std::ffi::CStr;
 
-impl HistEntry {
-    pub fn get_line<'a>(&'a self) -> &'a [u8] {
-        if self.line.is_null() { return &[0;0]; }
-        unsafe{ CStr::from_ptr((&self).line) }.to_bytes()
+    #[repr(C)]
+    struct HistEntry {
+        line: *const c_char,
+        timestamp: *const c_char,
+        data: *const c_char,
+    }
+
+    impl HistEntry {
+        pub fn get_line<'a>(&'a self) -> &'a [u8] {
+            if self.line.is_null() { return &[0;0]; }
+            unsafe{ CStr::from_ptr((&self).line) }.to_bytes()
+        }
+    }
+
+    pub fn history_each<F>(mut callback: F) where F: FnMut(&[u8]) {
+        let mut history = unsafe{ history_list() };
+        if history.is_null() { return; }
+
+        while ! unsafe{ (*history) }.is_null() {
+            let entry = unsafe{ &**history };
+            callback(entry.get_line());
+            history = unsafe{ history.offset(1) };
+        }
+    }
+
+    #[link(name = "readline")]
+    extern {
+        fn history_list() -> *const *const HistEntry;
+        pub fn rl_unix_line_discard(count: isize, key: isize) -> isize;
+        pub fn rl_refresh_line(count: isize, key: isize) -> isize;
+        pub fn rl_end_of_line(count: isize, key: isize) -> isize;
+        pub fn rl_insert_text(string: *const u8) -> isize;
     }
 }
 
-#[link(name = "readline")]
-extern {
-    fn history_list() -> *const *const HistEntry;
-    fn rl_unix_line_discard(count: isize, key: isize) -> isize;
-    fn rl_refresh_line(count: isize, key: isize) -> isize;
-    fn rl_end_of_line(count: isize, key: isize) -> isize;
-    fn rl_insert_text(string: *const u8) -> isize;
-}
-
-fn history_each<F>(mut callback: F) where F: FnMut(&[u8]) {
-    let mut history = unsafe{ history_list() };
-    if history.is_null() { return; }
-
-    while ! unsafe{ (*history) }.is_null() {
-        let entry = unsafe{ &**history };
-        callback(entry.get_line());
-        history = unsafe{ history.offset(1) };
-    }
-}
 
 #[no_mangle]
 pub extern fn rl_reverse_search_history(_direction: isize, _key: isize) -> isize {
@@ -52,14 +56,14 @@ fn custom_isearch() {
         .spawn().expect("could not find fzf");
     let mut stdin = process.stdin.unwrap();
 
-    history_each(|line| {
+    readline::history_each(|line| {
         stdin.write_all(line).unwrap();
         stdin.write_all(b"\n").unwrap();
     });
 
     process.stdin = Some(stdin);
     if ! process.wait().unwrap().success() {
-        unsafe{ rl_refresh_line(0, 0); }
+        unsafe{ readline::rl_refresh_line(0, 0); }
         return
     }
 
@@ -72,10 +76,10 @@ fn custom_isearch() {
     let buf = buf.as_ptr();
     unsafe {
         // clear line
-        rl_end_of_line(0, 0);
-        rl_unix_line_discard(0, 0);
-        rl_refresh_line(0, 0);
+        readline::rl_end_of_line(0, 0);
+        readline::rl_unix_line_discard(0, 0);
+        readline::rl_refresh_line(0, 0);
         // insert selected
-        rl_insert_text(buf);
+        readline::rl_insert_text(buf);
     }
 }
