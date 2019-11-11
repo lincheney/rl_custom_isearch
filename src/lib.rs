7b60b2ac2b49f18d7a44720145031911c49e0a60
林千里 <lincheney@gmail.com>
2019-11-11 21:07:36 +1100

    expose dynlib errors

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,20 @@ extern crate libc;
 use std::io::{Write, Read};
 use std::process::{Command, Stdio};
 
+type DynlibResult<T> = Result<T, &'static str>;
+
+macro_rules! dump_error {
+    ($result:expr, $default:expr) => {
+        match $result {
+            Ok(x) => x,
+            Err(e) => { eprintln!("{}", e); return $default },
+        }
+    }
+}
+
 macro_rules! dynlib_call {
     ($func:ident($($args:expr),*)) => {{
-        let ptr = {
-            use ::libc::$func;
-            $func($($args),*)
-        };
+        let ptr = libc::$func($($args),*);
         if ptr.is_null() {
             let error = ::libc::dlerror();
             if error.is_null() {
@@ -22,11 +30,6 @@ macro_rules! dynlib_call {
             Ok(ptr)
         }
     }}
-}
-
-macro_rules! dlopen {
-    ($name:expr) => { dlopen!($name, ::libc::RTLD_LAZY) };
-    ($name:expr, $flags:expr) => { dynlib_call!(dlopen($name.as_ptr() as _, $flags)) };
 }
 
 macro_rules! dlsym {
@@ -67,34 +70,32 @@ mod readline {
     use std::ffi::CStr;
     pub use self::lib::{HistEntry, rl_reverse_search_history};
 
-    pub fn get_history() -> *const *const HistEntry {
-        unsafe{ lib::history_list() }
+    pub fn get_history() -> ::DynlibResult<*const *const HistEntry> {
+        Ok(unsafe{ (*lib::history_list)?() })
     }
 
-    pub fn refresh_line() {
-        unsafe{ lib::rl_refresh_line(0, 0) };
+    pub fn refresh_line() -> ::DynlibResult<()> {
+        unsafe{ (*lib::rl_refresh_line)?(0, 0) };
+        Ok(())
     }
 
-    pub fn set_text(buf: Vec<u8>) {
-        let ptr = buf.as_ptr();
+    pub fn set_text(buf: Vec<u8>) -> ::DynlibResult<()> {
         unsafe {
             // clear line
-            lib::rl_end_of_line(0, 0);
-            lib::rl_unix_line_discard(0, 0);
-            lib::rl_refresh_line(0, 0);
+            (*lib::rl_end_of_line)?(0, 0);
+            (*lib::rl_unix_line_discard)?(0, 0);
+            (*lib::rl_refresh_line)?(0, 0);
             // insert selected
-            lib::rl_insert_text(ptr as _);
+            (*lib::rl_insert_text)?(buf.as_ptr() as _);
         }
+        Ok(())
     }
 
-    pub fn get_readline_name() -> Option<&'static str> {
-        unsafe {
-            let name = (*lib::rl_readline_name.ptr()).ptr();
-            if name.is_null() {
-                None
-            } else {
-                CStr::from_ptr(name).to_str().ok()
-            }
+    pub fn get_readline_name() -> ::DynlibResult<Option<&'static str>> {
+        match lib::rl_readline_name.as_ref() {
+            Ok(n) if n.ptr().is_null() => Ok(None),
+            Ok(n) => Ok(unsafe{ CStr::from_ptr(n.ptr() as _) }.to_str().ok()),
+            Err(e) => Err(e)
         }
     }
 
@@ -119,23 +120,21 @@ mod readline {
 
         pub struct Pointer<T>(usize, PhantomData<T>);
         impl<T> Pointer<T> {
-            pub fn new(ptr: *mut T)    -> Self { Self(ptr as _, PhantomData) }
-            pub fn ptr(&self)        -> *mut T { self.0 as *mut T }
-            pub unsafe fn set(&self, value: T) { *self.ptr() = value; }
+            pub fn ptr(&self) -> *mut T { self.0 as *mut T }
         }
 
-        lazy_static! {
-            pub static ref libreadline: Pointer<libc::c_void> = Pointer::new(unsafe {
-                if dlsym!(libc::RTLD_DEFAULT, "rl_initialize", usize).is_ok() {
-                    libc::RTLD_DEFAULT
-                } else {
-                    dlopen!(b"libreadline.so\0").unwrap()
-                }
-            });
-        }
         macro_rules! readline_lookup {
             ($name:ident: $type:ty) => {
-                lazy_static! { pub static ref $name: $type = unsafe{ dlsym!(libreadline.ptr(), stringify!($name)) }.unwrap(); }
+                readline_lookup!($name: $type; libc::RTLD_DEFAULT);
+            };
+            ($name:ident: $type:ty; $handle:expr) => {
+                lazy_static! {
+                    pub static ref $name: ::DynlibResult<$type> = unsafe {
+                        dlsym!($handle, stringify!($name)).or_else(|_|
+                            dynlib_call!(dlopen(b"libreadline.so\0".as_ptr() as _, libc::RTLD_NOLOAD | libc::RTLD_LAZY))
+                            .and_then(|lib| dlsym!(lib, stringify!($name)))
+                        )};
+                }
             }
         }
 
@@ -151,27 +150,28 @@ mod readline {
 
 #[no_mangle]
 pub extern fn rl_custom_function(direction: isize, key: isize) -> isize {
-    if custom_isearch() { return 0; }
-    unsafe{ readline::rl_reverse_search_history(direction, key) }
+    if dump_error!(custom_isearch(), 0) { return 0 }
+    let func = dump_error!(*readline::rl_reverse_search_history, 0);
+    unsafe{ func(direction, key) }
 }
 
-fn custom_isearch() -> bool {
+fn custom_isearch() -> DynlibResult<bool> {
     let mut command = Command::new("rl_custom_isearch");
     command.stdin(Stdio::piped()).stdout(Stdio::piped());
 
     // pass the readline name to process
-    if let Some(name) = readline::get_readline_name() {
+    if let Some(name) = readline::get_readline_name()? {
         command.env("READLINE_NAME", name);
     }
 
     let mut process = match command.spawn() {
         Ok(process) => process,
         // failed to run, do default readline search
-        Err(_) => { return false },
+        Err(_) => { return Ok(false) },
     };
     let mut stdin = process.stdin.unwrap();
 
-    for entry in CArray::new(readline::get_history()) {
+    for entry in CArray::new(readline::get_history()?) {
         let line = entry.get_line();
         // break on errors (but otherwise ignore)
         if ! ( stdin.write_all(line).is_ok() && stdin.write_all(b"\n").is_ok() ) {
@@ -181,14 +181,14 @@ fn custom_isearch() -> bool {
 
     // pass back stdin for process to close
     process.stdin = Some(stdin);
-    match process.wait() {
+    Ok(match process.wait() {
         // failed to run, do default readline search
         Err(_) => {
             false
         },
         // exited with code != 0, leave line as is
         Ok(code) if ! code.success() => {
-            readline::refresh_line();
+            readline::refresh_line()?;
             true
         },
         Ok(_) => {
@@ -196,13 +196,13 @@ fn custom_isearch() -> bool {
             let mut buf: Vec<u8> = vec![];
             if stdout.read_to_end(&mut buf).is_err() {
                 // failed to read stdout, default to readline search
-                return false
+                return Ok(false)
             }
             // make sure buf is null terminated
             if *buf.last().unwrap_or(&1) != 0 { buf.push(0); }
 
-            readline::set_text(buf);
+            readline::set_text(buf)?;
             true
         }
-    }
+    })
 }
